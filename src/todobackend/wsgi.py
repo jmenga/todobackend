5a340c22b5e34594d4aba67cb597612a72b8d60d
Justin Menga <justin.menga@gmail.com>
2016-01-30 01:55:25 +1300

    Refactor configuration settings

--- src/todobackend/wsgi.py
+++ src/todobackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todobackend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todobackend.settings.base")
 
 application = get_wsgi_application()
