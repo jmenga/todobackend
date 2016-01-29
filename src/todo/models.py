f959c6a8f3c837cf8b4c9a6644eb01aad0c02df3
Justin Menga <justin.menga@gmail.com>
2016-01-30 01:46:25 +1300

    Add TodoItem model and migrations

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,5 +1,8 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+  title = models.CharField(max_length=256, null=True, blank=True)
+  completed = models.BooleanField(blank=True, default=False)
+  url = models.CharField(max_length=256, null=True, blank=True)
+  order = models.IntegerField(null=True, blank=True)
