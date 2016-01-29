9999f486d3fbcc9693f3b9b8802ee5a8dd575e7e
Justin Menga <justin.menga@gmail.com>
2016-01-30 01:59:14 +1300

    Add application test settings

---
+++ src/todobackend/settings/test.py
@@ -0,0 +1,15 @@
+from base import *
+import os
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE','todobackend'),
+        'USER': os.environ.get('MYSQL_USER','todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD','password'),
+        'HOST': os.environ.get('MYSQL_HOST','localhost'),
+        'PORT': os.environ.get('MYSQL_PORT','3306'),
+    }
+}
