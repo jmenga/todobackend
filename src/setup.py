88de39bad71830fff1c359ccc07114f41c2e6f16
Justin Menga <justin.menga@gmail.com>
2016-01-31 00:21:21 +1300

    Add setup.py file

---
+++ src/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup (
+  name                 = "todobackend",
+  version              = "0.1.0",
+  description          = "Todobackend Django REST service",
+  packages             = find_packages(),
+  include_package_data = True,
+  scripts              = ["manage.py"],
+  install_requires     = ["Django>=1.9,<2.0",
+                          "django-cors-headers>=1.1.0",
+                          "djangorestframework>=3.3.1",
+                          "MySQL-python>=1.2.5"],
+  extras_require       = {
+                            "test": [
+                              "colorama>=0.3.3",
+                              "coverage>=4.0.3",
+                              "django-nose>=1.4.2",
+                              "nose>=1.3.7",
+                              "pinocchio>=0.4.2"
+                            ]
+                         }
+)
