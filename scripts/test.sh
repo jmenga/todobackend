6295bf32204e675aab99df3ceb0e5ef0e5184302
Justin Menga <justin.menga@gmail.com>
2016-02-01 18:52:25 +1300

    Add support for build cache and add builder service for building application artifacts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,11 @@
 # Activate virtual environment
 . /appenv/bin/activate
 
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
