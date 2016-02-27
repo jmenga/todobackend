185db91a1f4c59aa7018f8cb1ffb837f432478b2
Justin Menga <justin.menga@gmail.com>
2016-02-27 23:59:47 +1300

    Fix incorrect create test status code assertion

--- src/todo/tests.py
+++ src/todo/tests.py
@@ -17,7 +17,7 @@ class TestCreateTodoItem(APITestCase):
     self.response = createItem(self.client)
 
   def test_received_201_created_status_code(self):
-    self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+    self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
   def test_received_location_header_hyperlink(self):
     self.assertRegexpMatches(self.response['Location'], '^http://.+/todos/[\d]+$')
